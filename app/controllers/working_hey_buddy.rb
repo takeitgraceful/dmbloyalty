908b0b483a402696d81916f2dc2dd3bad663871c
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-01-13 22:19:13 -0500

    Removing Port

--- app/controllers/working_hey_buddy.rb
+++ app/controllers/working_hey_buddy.rb
@@ -2,7 +2,7 @@ require 'socket'
 require 'rexml/document'
 include REXML
 
-server = TCPServer.new 3000 # Server bind to port 2000
+server = TCPServer.new 4444 # Server bind to port 2000
 loop do
   client = server.accept    # Wait for a client to connect
   client.puts "Hey Buddy 2 !"
