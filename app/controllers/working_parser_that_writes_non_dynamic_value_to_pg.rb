191b1ab724077c5434ec184fb811c5341bf98384
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-02-10 16:30:06 -0500

    Writing Card Numbers to Database

--- app/controllers/working_parser_that_writes_non_dynamic_value_to_pg.rb
+++ app/controllers/working_parser_that_writes_non_dynamic_value_to_pg.rb
@@ -1,8 +1,6 @@
 require 'nokogiri'
 require 'rubygems'
 require 'pg'
-require 'active_support'
-require 'active_support/core_ext'
 
 conn = PG.connect( dbname: 'my_database_development' )
 
@@ -12,14 +10,11 @@ open("dmbloyalty.html") do |file|
     changes = file.read
     parsed_data = Nokogiri::HTML.parse(changes)
     unique_data = parsed_data.xpath("//*[@state='Paid']/@manager_uid")
-    @card_test = unique_data
+    @card_test = 2222444
     #@card_test = unique_data
     #example = conn.exec("INSERT INTO cards(card_used,created_at,updated_at) VALUES (#{@card_test},NOW(),NOW());")
     unless changes.empty?  # file.read returns "" if there is not more data to read
-      print unique_data
-    end
-    if unique_data.present?
-      conn.exec("INSERT INTO cards(card_used,created_at,updated_at) VALUES (#{@card_test},NOW(),NOW());")
+      print unique_data && conn.exec("INSERT INTO cards(card_used,created_at,updated_at) VALUES (#{@card_test},NOW(),NOW());")  # reads only a additional part of the file
     end
     sleep 1.0        # sleep for a second; without it script would use 100% of processor
   end
