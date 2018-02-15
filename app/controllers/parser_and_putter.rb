b2988f76c3c34d7f7be36fe86611b862aaa8c02f
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-02-15 02:42:03 -0500

    Updates to database

--- app/controllers/parser_and_putter.rb
+++ app/controllers/parser_and_putter.rb
@@ -4,7 +4,7 @@ require 'pg'
 require 'active_support'
 require 'active_support/core_ext'
 
-#conn = PG.connect(ENV['DATABASE_URL'])
+conn = PG.connect(ENV['DATABASE_URL'])
 
 open("dmbloyalty.html") do |file|
   file.seek(0, IO::SEEK_END) # rewinds file to the end
