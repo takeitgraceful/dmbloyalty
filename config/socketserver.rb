c8e9c3d9132c8d6bf8f73b28d278ea04fc0b4de1
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-02-15 10:42:56 -0500

    Troubleshooting Scripts

---
+++ config/socketserver.rb
@@ -0,0 +1,18 @@
+#require 'socket'
+require 'nokogiri'
+require 'rubygems'
+
+#server = TCPServer.new 4444
+html        = File.open("dmbloyalty.html")
+parsed_data = Nokogiri::HTML.parse(html)
+
+#loop do
+#  Thread.start(server.accept) do |client|
+    while line = client.gets
+      # open and write to a file with ruby
+      open('dmbloyalty.html', 'a+') { |f|
+        f.puts line}
+    end
+  client.close
+end
+end
