2b1436579e85e872551dddcb92b0841590b85d68
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-02-17 23:37:58 -0500

    Working Nanobox

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,1 @@
-User.create!(name:  "Takeitgraceful",
-             email: "takeitgraceful@gmail.com",
-             password:              "c3H-i-eof",
-             password_confirmation: "c3H-i-eof",
-             admin: true)
+
