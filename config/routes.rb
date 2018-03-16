9ba11d4428cac86e32f8cdc628d425c6f8ebd969
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-03-16 01:50:06 -0400

    Loyalty Rankings

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
 
   root 'static_pages#home'
-  get  '/dmbexample', to: 'cards#dmbexample'  
+  get  '/dmbexample', to: 'cards#dmbexample'
+  get  '/dmbexample2', to: 'cards#dmbexample2'  
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
